4a56e0262890de3a0b316da2df9ec04b41365e3e
Hanna Garcia Guerra <hnngarcia11@gmail.com>
2025-08-30 14:56:54 -0500

    feat: Integration of the basic structure of the contact module

---
+++ src/contacto/models/contacto.java
@@ -0,0 +1,50 @@
+package contacto.models;
+
+public class contacto {
+    private String nombre;
+    private String correo;
+    private String asunto;
+    private String mensaje;
+
+    public contacto(String nombre, String correo, String asunto, String mensaje) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.asunto = asunto;
+        this.mensaje = mensaje;
+    }
+
+    //setters
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public void setCorreo(String correo){
+        this.correo = correo;
+    }
+
+    public void setAsunto(String asunto){
+        this.asunto = asunto;
+    }
+
+    public void setMensaje(String mensaje){
+        this.mensaje = mensaje;
+    }
+
+    //Getters
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getCorreo(){
+        return correo;
+    }
+
+    public String getAsunto(){
+        return asunto;
+    }
+
+    public String getMensaje(){
+        return mensaje;
+    }
+}
